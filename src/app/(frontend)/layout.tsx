5bb6795774babe69dd3f1ee565b25d816f4a79f5
igomeow <ishu.cxx@gmail.com>
2025-09-25 17:16:17 +0530

    production

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,14 +1,6 @@
 import './styles.css'
 import { SessionProviderWrapper } from '@/components/providers/SessionProviderWrapper'
 import { ThemeProvider } from '@/components/theme-provider'
-import { AppSidebar } from '@/components/app-sidebar'
-import { SiteHeader } from '@/components/site-header'
-import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
-
-export const metadata = {
-  description: 'Vortex core ai',
-  title: 'VortexTracker',
-}
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -20,22 +12,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           enableSystem
           disableTransitionOnChange
         >
-          <SessionProviderWrapper>
-            <SidebarProvider
-              style={
-                {
-                  '--sidebar-width': 'calc(var(--spacing) * 72)',
-                  '--header-height': 'calc(var(--spacing) * 12)',
-                } as React.CSSProperties
-              }
-            >
-              <AppSidebar variant="inset" />
-              <SidebarInset>
-                <SiteHeader />
-                {children}
-              </SidebarInset>
-            </SidebarProvider>
-          </SessionProviderWrapper>
+          <SessionProviderWrapper>{children}</SessionProviderWrapper>
         </ThemeProvider>
       </body>
     </html>
--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,21 +1,7 @@
-// app/dashboard/page.tsx
-import data from './data.json'
-import { ChartAreaInteractive } from '@/components/chart-area-interactive'
-import { DataTable } from '@/components/data-table'
-import { SectionCards } from '@/components/section-cards'
-
-export default async function Dashboard() {
+function page() {
   return (
-    <div className="flex flex-1 flex-col">
-      <div className="@container/main flex flex-1 flex-col gap-2">
-        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-          <SectionCards />
-          <div className="px-4 lg:px-6">
-            <ChartAreaInteractive />
-          </div>
-          <DataTable data={data} />
-        </div>
-      </div>
-    </div>
+    <div>page</div>
   )
 }
+
+export default page;
