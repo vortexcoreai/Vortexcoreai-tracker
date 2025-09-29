01c032ba66d568d6f15e7f7cad0199221237f479
igomeow <ishu.cxx@gmail.com>
2025-09-29 17:56:30 +0530

    'production'

--- src/app/(frontend)/dashboard/attendance/page.tsx
+++ src/app/(frontend)/dashboard/attendance/page.tsx
@@ -1,7 +1,11 @@
 import { apiFetch } from '@/lib/api'
 import AttendanceTable from '@/components/attendance-table'
 
-export default async function Page() {
-  const data = await apiFetch('/api/attendance')
+export default async function Page({ searchParams }) {
+  console.log(searchParams)
+  const year = Number(searchParams.year) || new Date().getFullYear()
+  const month = Number(searchParams.month) || new Date().getMonth() + 1
+
+  const data = await apiFetch(`/api/attendance?where[date][greater_than_equal]=${year}-${month}-01&where[date][less_than_equal]=${year}-${month}-31&limit=100&sort=date`)
   return <AttendanceTable initialData={data.docs || []} />
 }
