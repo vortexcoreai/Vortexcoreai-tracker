53bdd20891f0be9743371472fd42da63c74359a6
igomeow <ishu.cxx@gmail.com>
2025-10-03 23:51:01 +0530

    'production'

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 
-// import { postgresAdapter } from '@payloadcms/db-postgres'
-import { sqliteAdapter } from '@payloadcms/db-sqlite'
+import { postgresAdapter } from '@payloadcms/db-postgres'
+// import { sqliteAdapter } from '@payloadcms/db-sqlite'
 
 import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
@@ -32,17 +32,17 @@ export default buildConfig({
     outputFile: path.resolve(dirname, 'payload-types.ts'),
   },
 
-  // db: postgresAdapter({
-  //   pool: {
-  //     connectionString: process.env.DATABASE_URI || '',
-  //   },
-  // }),
-
-  db: sqliteAdapter({
-    client: {
-      url: `file:${path.resolve(dirname, 'dev.db')}`,
+  db: postgresAdapter({
+    pool: {
+      connectionString: process.env.DATABASE_URI || '',
     },
   }),
+
+  // db: sqliteAdapter({
+  //   client: {
+  //     url: `file:${path.resolve(dirname, 'dev.db')}`,
+  //   },
+  // }),
 
   plugins: [payloadCloudPlugin()],
 })
